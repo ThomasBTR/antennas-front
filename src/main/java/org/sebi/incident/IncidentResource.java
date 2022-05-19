0d1be3dc46a46b99a0da41826d7ec2d12cad845f
ThomasBTR <thomatito971@gmail.com>
2022-05-19 12:26:00 +0000

    Add web IncidentWebResouce to conect front and backend

--- src/main/java/org/sebi/incident/IncidentResource.java
+++ src/main/java/org/sebi/incident/IncidentResource.java
@@ -2,14 +2,26 @@ package org.sebi.incident;
 
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 @Path("rest/incidents")
-public class IncidentResource {
+public class IncidentResource{
     
+    @Inject
+    @RestClient
+    IncidentWebResouce incidentWebResouce;
+
+    @ConfigProperty(name = "secret.apiKey", defaultValue = "koukou")
+    private String apiKey;
+
+
     @GET
     public List<Incident> getIncidents(){
-       return Incident.listAll();
+       return incidentWebResouce.getIncidents(apiKey);
     } 
 }
